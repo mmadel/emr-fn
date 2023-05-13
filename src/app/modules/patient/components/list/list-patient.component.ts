29cb8709980c71b944abcf9ecfaec28b9fbc0830
mmadel <mohammed.adel82@gmail.com>
2023-05-13 11:45:17 +0300

    add patient'c card , impc route to create c

--- src/app/modules/patient/components/list/list-patient.component.ts
+++ src/app/modules/patient/components/list/list-patient.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-patient',
@@ -7,12 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPatientComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  create(){
-    
+  create() {
+    this.router.navigateByUrl('/patient/create');
   }
 }
