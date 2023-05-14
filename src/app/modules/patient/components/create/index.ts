b6a080928cfb3a60c6e424d06a8925b7afe2524b
mmadel <mohammed.adel82@gmail.com>
2023-05-14 14:50:00 +0300

    import patient dependencies components in module

---
+++ src/app/modules/patient/components/create/index.ts
@@ -0,0 +1,8 @@
+
+export * from './patient.basic.info/patient-basic-info.component'
+export * from './patient.id.info/patient-id-info.component'
+export * from './patient.contact.info/patient-contact-info.component'
+export * from './patient.emergency.info/patient-emergency-info.component'
+export * from './patient.insurance.info/patient-insurance-info.component'
+export * from './patient.clinic.info/patient-clinic-info.component'
+export * from './patient.case.info/patient-case-info.component'
