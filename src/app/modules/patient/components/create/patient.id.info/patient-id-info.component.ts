20e5b6c4ef3bd6ada55d4bd00cecb4b5ef3ff4ab
mmadel <mohammed.adel82@gmail.com>
2023-05-17 13:36:37 +0300

    make idcomponent extends basic asn add form tag

--- src/app/modules/patient/components/create/patient.id.info/patient-id-info.component.ts
+++ src/app/modules/patient/components/create/patient.id.info/patient-id-info.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { IdType } from 'src/app/modules/common/models/enums/id.type';
+import { BasicComponent } from 'src/app/util/basic.component';
 import { Patient } from '../../../models/patient';
 
 @Component({
@@ -7,10 +9,14 @@ import { Patient } from '../../../models/patient';
   templateUrl: './patient-id-info.component.html',
   styleUrls: ['./patient-id-info.component.css']
 })
-export class PatientIdInfoComponent implements OnInit {
+export class PatientIdInfoComponent extends BasicComponent implements OnInit, AfterViewInit {
+  @ViewChild('patientIdForm') patientIdForm: NgForm;
   @Input() patient: Patient;
-  idTypes= IdType;
-  constructor() { }
+  idTypes = IdType;
+  constructor() { super() }
+  ngAfterViewInit(): void {
+    this.setForm(this.patientIdForm)
+  }
 
   ngOnInit(): void {
   }
