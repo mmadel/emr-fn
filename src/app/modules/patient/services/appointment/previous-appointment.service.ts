9163744b9607214a9d6ca25f5c79d6fd0acce2bc
mmadel <mohammed.adel82@gmail.com>
2023-05-19 18:22:03 +0300

    create function to get Previous Appointment(s)

--- src/app/modules/patient/services/appointment/previous-appointment.service.ts
+++ src/app/modules/patient/services/appointment/previous-appointment.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { IApiParams } from 'src/app/modules/common/interfaces/api.params';
+import { BasePaginationService } from 'src/app/modules/common/service/base-pagination.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PreviousAppointmentService {
+export class PreviousAppointmentService extends BasePaginationService {
+  private baseUrl = environment.baseURL + 'appointment/chart/previous'
+  constructor(httpClient: HttpClient) {
+    super(httpClient);
+  }
 
-  constructor() { }
+  public findAllPreviousAppointments(config$: BehaviorSubject<IApiParams>, pateintId: number): Observable<any> {
+    return this.get(config$, this.baseUrl + '/find/patientId/' + pateintId + '/clinicId/' + 2)
+  }
+
+  public findPreviousAppointmentsByCase(config$: BehaviorSubject<IApiParams>,
+    pateintId: number,
+    caseId: number): Observable<any> {
+    return this.get(config$, this.baseUrl + '/find/patientId/' + pateintId + '/clinicId/' + 1 + '/patientCaseId/' + caseId);
+
+  }
 }
